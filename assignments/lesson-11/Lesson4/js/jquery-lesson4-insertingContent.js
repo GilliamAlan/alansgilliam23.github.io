a4a641c388e47d9baf1cc1b9f798f0070e0075e6
alansgilliam23 <alansgilliam23@gmail.com>
2018-12-02 14:43:05 -0700

    update

---
+++ assignments/lesson-11/Lesson4/js/jquery-lesson4-insertingContent.js
@@ -0,0 +1,11 @@
+$(function () {
+    // jQuery Code goes here
+    var nameVal = $('input[name="nameVal"]');
+
+    function FillEmpty() {
+        if (nameVal.val() == '') {
+            nameVal.val('John Doe');
+        }
+    }
+    $('#ReqAQuoteBtn').click(FillEmpty);
+})
