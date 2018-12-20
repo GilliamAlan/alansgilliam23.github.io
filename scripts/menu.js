5143efcd5bfc8b81fc8787f00d23c136ecf3e0e7
alansgilliam23 <alansgilliam23@gmail.com>
2018-12-19 20:55:08 -0700

    update

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,20 @@
-function menu() {
-    document.getElementsByClassName("headernav")[0].classList.toggle("responsive");
+
+function openMenu() {
+    document.getElementById("menu").classList.toggle("hide");
+}
+
+function showTucson() {
+    document.getElementById("show-tucson").classList.toggle("hide-tucson");
+}
+
+function showPhoenix() {
+    document.getElementById("show-phoenix").classList.toggle("hide-phoenix");
+}
+
+function showGilbert() {
+    document.getElementById("show-gilbert").classList.toggle("hide-gilbert");
+}
+
+function showGila() {
+    document.getElementById("show-gila").classList.toggle("hide-gila");
 }
